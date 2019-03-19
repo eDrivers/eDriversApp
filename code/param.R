765c2970a49fd8e8e10fd215d0253dc04b56b20f
David Beauchesne <david.beauchesne@uqar.ca>
2019-03-19 09:41:50 -0400

    add colors in parameter call

--- code/param.R
+++ code/param.R
@@ -29,6 +29,9 @@ layers$checkbox <- sprintf('<input type="checkbox" name="%s" value="%s"/>',
                            layers$FileName,
                            1:nDr)
 
+# Colors
+cols <- c('#C7CBCE','#96A3A3','#687677','#222D3D','#25364A','#C77F20','#E69831','#E3AF16','#E4BE29','#F2EA8B')
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~   FONTS   ~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # font_add_google(name = 'Josefin Slab')
